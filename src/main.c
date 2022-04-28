95028f6a2c88266f89e50cc18723208af9d3f5c0
bitten 1up <seant9140@gmail.com>
2022-04-28 17:56:06 -0500

    pwm test

--- src/main.c
+++ src/main.c
@@ -26,10 +26,17 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include "pico/stdlib.h"   // stdlib 
+#include "hardware/irq.h"  // interrupts
+#include "hardware/pwm.h"  // pwm 
+#include "hardware/sync.h" // wait for interrupt 
 
 #include "bsp/board.h"
 #include "tusb.h"
 #include "usb_descriptors.h"
+#define AUDIO_PIN 28
+#include "sample.h"
+int wav_position = 0;
 
 //--------------------------------------------------------------------+
 // MACRO CONSTANT TYPEDEF PROTYPES
@@ -50,10 +57,57 @@ static uint32_t blink_interval_ms = BLINK_NOT_MOUNTED;
 
 void led_blinking_task(void);
 void video_task(void);
-
+/*------------- PWMI --------------*/
+void pwm_interrupt_handler() {
+    pwm_clear_irq(pwm_gpio_to_slice_num(AUDIO_PIN));    
+    if (wav_position < (WAV_DATA_LENGTH<<3) - 1) { 
+        // set pwm level 
+        // allow the pwm value to repeat for 8 cycles this is >>3 
+        pwm_set_gpio_level(AUDIO_PIN, WAV_DATA[wav_position>>3]);  
+        wav_position++;
+    } else {
+        // reset to start
+        wav_position = 0;
+    }
+}
 /*------------- MAIN -------------*/
 int main(void)
 {
+  
+  /* Overclocking for fun but then also so the system clock is a 
+   * multiple of typical audio sampling rates.
+   */
+  stdio_init_all();
+  set_sys_clock_khz(176000, true); 
+  gpio_set_function(AUDIO_PIN, GPIO_FUNC_PWM);
+
+  int audio_pin_slice = pwm_gpio_to_slice_num(AUDIO_PIN);
+
+  // Setup PWM interrupt to fire when PWM cycle is complete
+  pwm_clear_irq(audio_pin_slice);
+  pwm_set_irq_enabled(audio_pin_slice, true);
+  // set the handle function above
+  irq_set_exclusive_handler(PWM_IRQ_WRAP, pwm_interrupt_handler); 
+  irq_set_enabled(PWM_IRQ_WRAP, true);
+
+  // Setup PWM for audio output
+  pwm_config config = pwm_get_default_config();
+  /* Base clock 176,000,000 Hz divide by wrap 250 then the clock divider further divides
+   * to set the interrupt rate. 
+   * 
+   * 11 KHz is fine for speech. Phone lines generally sample at 8 KHz
+   * 
+   * 
+   * So clkdiv should be as follows for given sample rate
+   *  8.0f for 11 KHz
+   *  4.0f for 22 KHz
+   *  2.0f for 44 KHz etc
+   */
+  pwm_config_set_clkdiv(&config, 8.0f); 
+  pwm_config_set_wrap(&config, 250); 
+  pwm_init(audio_pin_slice, &config, true);
+
+  pwm_set_gpio_level(AUDIO_PIN, 0);
   board_init();
   tusb_init();
 
